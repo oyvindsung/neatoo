11714675bbc91e57d9c5bef79ef76613e9dc364d
oyvindsung <songzle@mail2.sysu.edu.cn>
2025-06-10 23:50:02 +0800

    Add MixView()

--- neatoo/View_Tab_Mix.swift
+++ neatoo/View_Tab_Mix.swift
@@ -16,3 +16,86 @@ struct MixView: View {
 #Preview {
     MixView()
 }
+
+import SwiftUI
+import SwiftData
+
+struct FinanceView: View {
+    @Query private var payments: [Payment]
+    
+    @Environment(\.modelContext) private var context
+    
+    @State private var showAddPaymentSheet = false
+    @State private var isImporting = false
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                TimeChartView()
+                CardView(
+                    title: "支出",
+                    items: payments.sorted { $0.date > $1.date },
+                    showAddSheet: $showAddPaymentSheet,
+                    itemName: { $0.name },
+                    rowDestination: { payment in PaymentDetailView(payment: payment) },
+                    allItemsView: { AllPaymentListView(
+                        payments: payments,
+                        toDetail: { payment in PaymentDetailView(payment: payment) },
+                        delete: { indexSet in
+                            for index in indexSet {
+                                context.delete(payments[index])
+                            }
+                        },
+                        filename: "payment_data"
+                    )
+                    .modelContainer(for: Payment.self)
+                    },
+                    addItemView: {
+                        AddNewPaymentView { newPayment in context.insert(newPayment) }
+                    }
+                )
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        isImporting = true
+                    } label: {
+                        Image(systemName: "square.and.arrow.up")
+                    }
+                }
+            }
+            .fileImporter(
+                isPresented: $isImporting,
+                allowedContentTypes: [.json]
+            ) { result in
+                do {
+                    let fileURL = try result.get()
+                    guard fileURL.startAccessingSecurityScopedResource() else {
+                        print("没有读取权限")
+                        return
+                    }
+                    defer { fileURL.stopAccessingSecurityScopedResource() }
+                    
+                    let data = try Data(contentsOf: fileURL)
+                    let decoder = JSONDecoder()
+                    decoder.dateDecodingStrategy = .iso8601
+
+                    let decodedItems = try decoder.decode([Payment].self, from: data)
+                    for item in decodedItems {
+                        withAnimation {
+                            context.insert(item)
+                        }
+                    }
+                } catch {
+                    print("Import failed: \(error)")
+                }
+            }
+            .navigationTitle("记账")
+        }
+    }
+}
+
+#Preview {
+    FinanceView()
+        .modelContainer(for: [Payment.self])
+}
